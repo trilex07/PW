5a298a5932c3b41b2a8d2b75755da087d59fa36a
trilex07 <trilex07@gmail.com>
2020-01-31 15:24:48 +0100

    Aplikacja dla powiatu

---
+++ src/bezkresk/Bezkresk.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bezkresk;
+
+/**
+ *
+ * @author trile
+ */
+public class Bezkresk {
+
+    private static void STart() {
+        OknoMenu okno_start = new OknoMenu();
+        okno_start.setVisible(true);}
+  
+
+   
+  
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+   STart();
+    }
+}
